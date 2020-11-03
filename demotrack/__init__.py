c83927a0b2f2cc4c302dbf47bd28be50593fcb8a
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-11-03 13:43:25 +0100

    moves setup program function to opencl module

--- demotrack/__init__.py
+++ demotrack/__init__.py
@@ -2,4 +2,4 @@ __version__ = "1.0.0"
 
 from .particle import Particle, ParticleSet
 from .beam_elements import Lattice
-from .opencl import setup_pyopencl_ctx
+from .opencl import setup_pyopencl_ctx, get_track_particle_program
